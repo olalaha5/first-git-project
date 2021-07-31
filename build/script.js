7d2b7fc07a601556b2dfb5b991b29c93c4cd843c
Ilya Olkhovsky <ilya20050@gmail.com>
2021-07-31 18:31:37 +0300

    added adaptive slider in 6th block

--- build/script.js
+++ build/script.js
@@ -19,3 +19,43 @@ btnPause.addEventListener('click', () => {
     btnPause.style.display = 'none';
     help.innerHTML = 'Click to play the video'
 });
+
+// ------------------------------------------------
+
+let n = 0
+function positionTriangle(i) {
+    n = i;
+    if (document.documentElement.clientWidth < 561) {
+        if (n < 2) positionTriangleTop(document.querySelectorAll('.our-team__card')[n]);
+        else positionTriangleBottom(document.querySelectorAll('.our-team__card')[n]);
+    } else {
+        positionTriangleTop(document.querySelectorAll('.our-team__card')[n]);
+    }
+}
+
+function positionTriangleTop(elem) {
+    let triangle = document.querySelector('.our-team__triangle');
+    triangle.style.transform = 'rotate(0deg)';
+    triangle.style.left = `${elem.offsetWidth / 2 + elem.offsetLeft - 40}px`;
+    triangle.style.top = '-69px';
+}
+
+function positionTriangleBottom(elem) {
+    let triangle = document.querySelector('.our-team__triangle');
+    triangle.style.transform = 'rotate(180deg)';
+    triangle.style.left = `${elem.offsetWidth / 2 + elem.offsetLeft - 40}px`;
+    triangle.style.top = `${document.querySelector('.our-team__container').offsetHeight + 68}px`;
+}
+
+window.addEventListener('load', () => {
+    positionTriangle(0)
+});
+
+window.addEventListener('resize', () => {
+    positionTriangle(n)
+});
+
+for (let i = 0; i < document.querySelectorAll('.our-team__card').length; i++) {
+    document.querySelectorAll('.our-team__card')[i].addEventListener('click', () => positionTriangle(i))
+}
+
