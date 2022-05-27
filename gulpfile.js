2214a531d3269826d6a78b474b9491d58fedc82f
Ilya Olkhovsky <ilya20050@gmail.com>
2022-05-27 17:33:12 +0300

    added some changes

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ const csso = require('gulp-csso');
 const browsersync = require('browser-sync').create();
 const imagemin = require('gulp-imagemin');
 const clean = require('gulp-clean');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const pug = require('gulp-pug');
 const autoprefixer = require('gulp-autoprefixer');
@@ -21,6 +21,8 @@ const shorthand = require('gulp-shorthand');
 const imageminJpegRecompress = require('imagemin-jpeg-recompress');
 const pngquant = require('imagemin-pngquant');
 const flatten = require('gulp-flatten');
+const imageResize = require('gulp-image-resize');
+const rename = require("gulp-rename");
 
 
 
@@ -49,16 +51,38 @@ function fullCssCompilation() {
 
 
 
-function img() {
-    return src('./src/**/*.png') //Выберем наши картинки
-        .pipe(flatten({ includeParents: 0 }))
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{ removeViewBox: false }],
-            use: [pngquant()],
-            interlaced: true
-        }))
-        .pipe(dest('./build/img'));
+// function img() {
+//     return src('./src/**/*.png') //Выберем наши картинки
+//         .pipe(flatten({ includeParents: 0 }))
+//         .pipe(imagemin([
+//             pngquant({
+//                 quality: [0.1, 0.2]
+//             })
+//         ], {
+//             verbose: true
+//         }))
+//         .pipe(dest('./build/img'));
+// }
+
+
+
+function img(cb) {
+    [300, 800, 1000, 2000].forEach(function (size) {
+        src('src/blocks/**/*.{jpg,jpeg,png}')
+            .pipe(flatten({ includeParents: 0 }))
+            .pipe(imagemin([
+                pngquant({
+                    quality: [0.7, 0.8]
+                })
+            ], {
+                verbose: true
+            }))
+            .pipe(dest('./build/img'))
+            .pipe(imageResize({ width: size }))
+            .pipe(rename(function (path) { path.basename = `${path.basename} ${size}w`; }))
+            .pipe(dest('./build/img__resize'))
+    });
+    cb()
 }
 
 
@@ -91,3 +115,4 @@ function browserSync() {
 
 exports.watch = parallel(watchFiles, browserSync);
 exports.default = series(clear, parallel(fullHtmlCompilation, fullCssCompilation, img));
+exports.image = parallel(img);
